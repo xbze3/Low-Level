07311d3480d5ecf90c14f48115398b0b0e893161
xbze3 <ezranminty@gmail.com>
2025-02-06 02:27:33 -0400

    Update
    Most stable memory allocation version
    From all tests, everything seems to work
    Will do more tests to confirms

--- src/xb_mloc/xb_mloc_1.c
+++ src/xb_mloc/xb_mloc_1.c
@@ -14,6 +14,12 @@
 
 // I added functionlity to ensure that memory allocations are alligned to a 16 byte boundary
 
+// I think I got everything working but I'm not entirely sure
+// Will do more testing to verify
+
+// From all the basic tests that I've run it seems like it works
+// Will do more testing at a later date to confirm
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -26,34 +32,31 @@ typedef struct Block
     int size;
     int free;
     struct Block *next;
+    struct Block *prev;
 } Block;
 
-Block *mem_init(int size)
+static Block *free_list_head = NULL;
+
+void *xb_mloc(int size)
 {
     if (size <= 0)
         return NULL;
 
     size = ALIGN_SIZE(size);
 
-    Block *block = malloc(sizeof(Block));
-    if (block == NULL)
-        return NULL;
+    if (free_list_head == NULL)
+    {
+        free_list_head = (Block *)sbrk(sizeof(Block));
+        if (free_list_head == (void *)-1)
+            return NULL;
 
-    block->size = size;
-    block->free = 1;
-    block->next = NULL;
+        free_list_head->size = ALIGN_SIZE(3000);
+        free_list_head->free = 1;
+        free_list_head->next = NULL;
+        free_list_head->prev = NULL;
+    }
 
-    return block;
-}
-
-Block *xb_mloc(Block *block, int size)
-{
-    if (size <= 0)
-        return NULL;
-
-    size = ALIGN_SIZE(size);
-
-    Block *tmp = block;
+    Block *tmp = free_list_head;
     Block *worstFit = NULL;
 
     while (tmp)
@@ -75,67 +78,75 @@ Block *xb_mloc(Block *block, int size)
 
     if (worstFit->size > size)
     {
-        Block *new_block = malloc(sizeof(Block));
-        if (new_block == NULL)
+        Block *new_block = (Block *)sbrk(sizeof(Block));
+        if (new_block == (void *)-1)
             return NULL;
 
         new_block->size = ALIGN_SIZE(worstFit->size - size);
         new_block->free = 1;
         new_block->next = worstFit->next;
+        new_block->prev = worstFit;
+
+        if (new_block->next)
+            new_block->next->prev = new_block;
 
         worstFit->size = size;
         worstFit->next = new_block;
+
+        // printf("Worst Fit Block: %p\n", worstFit);
     }
 
-    return worstFit;
+    return (void *)(worstFit + sizeof(Block));
 }
 
-int xb_free(Block *block, Block *rem_block)
+int xb_free(void *rem_block)
 {
-    if (block == NULL || rem_block == NULL)
-        return -1;
-
-    Block *prev = NULL;
-    Block *tmp = block;
-
-    while (tmp && tmp != rem_block)
+    if (rem_block == NULL)
     {
-        prev = tmp;
-        tmp = tmp->next;
-    }
-
-    if (tmp == NULL)
-    {
-        printf("Error: Block not found.\n");
         return -1;
     }
 
-    tmp->free = 1;
+    Block *block_to_free = (Block *)rem_block - sizeof(Block);
+    block_to_free->free = 1;
 
-    if (tmp->next && tmp->next->free == 1)
+    // printf("Free: %p\n", block_to_free);
+
+    // printf("Freeing block at %p with size %d\n", block_to_free, block_to_free->size);
+
+    if (block_to_free->next && block_to_free->next->free == 1)
     {
-        tmp->size += tmp->next->size;
-        Block *to_free = tmp->next;
-        tmp->next = tmp->next->next;
-        free(to_free);
+        // printf("Coalescing with next block at %p\n", block_to_free->next);
+        block_to_free->size += block_to_free->next->size;
+        Block *to_free = block_to_free->next;
+        block_to_free->next = block_to_free->next->next;
+        if (block_to_free->next)
+        {
+            block_to_free->next->prev = block_to_free;
+        }
+        sbrk(-(sizeof(Block) + to_free->size));
     }
 
-    if (prev && prev->free == 1)
+    if (block_to_free->prev && block_to_free->prev->free == 1)
     {
-        prev->size += tmp->size;
-        prev->next = tmp->next;
-        free(tmp);
+        // printf("Coalescing with previous block at %p\n", block_to_free->prev);
+        block_to_free->prev->size += block_to_free->size;
+        block_to_free->prev->next = block_to_free->next;
+        if (block_to_free->next)
+        {
+            block_to_free->next->prev = block_to_free->prev;
+        }
+        sbrk(-(sizeof(Block) + block_to_free->size));
     }
 
     return 0;
 }
 
-int printMem(Block *block)
+int printMem()
 {
-    if (block == NULL)
-        return -1;
+    Block *tmp = free_list_head;
 
-    Block *tmp = block;
+    if (tmp == NULL)
+        return -1;
 
     while (tmp)
     {
@@ -150,25 +161,24 @@ int printMem(Block *block)
 
 int main()
 {
-    Block *memory = mem_init(3000);
+    int *test = xb_mloc(sizeof(int));
 
-    Block *new1 = xb_mloc(memory, 100);
-    Block *new2 = xb_mloc(memory, 300);
-    Block *new3 = xb_mloc(memory, 200);
-    Block *new4 = xb_mloc(memory, 50);
-    Block *new5 = xb_mloc(memory, 70);
+    *test = 4;
 
-    printMem(memory);
+    // printf("Test: %p\n", (Block *)test - sizeof(Block));
 
-    xb_free(memory, new1);
-    xb_free(memory, new2);
-    xb_free(memory, new3);
-    xb_free(memory, new4);
-    xb_free(memory, new5);
+    char *test2 = xb_mloc(sizeof(char));
+    short *test3 = xb_mloc(sizeof(short));
+
+    printMem();
+
+    xb_free(test);
+    xb_free(test2);
+    xb_free(test3);
 
     printf("\n");
 
-    printMem(memory);
+    printMem();
 
     return 0;
 }
